3035acca84a7348d34aecc53d30339726c3a8051
Lee <leemcf4@hotmail.co.uk>
2021-01-17 20:43:26 +0000

    test v14

--- backend/server.js
+++ backend/server.js
@@ -42,8 +42,8 @@ app.use((err, req, res, next) => {
 })
 
 const port = process.env.PORT || 5000
-app.set("port", port)
+//app.set("port", port)
 
-//app.listen(port, () => {
-//  console.log(`serve at http://localhost:${port}`)
-//})
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`)
+})
