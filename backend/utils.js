2bb8826ebc0ef356468127839c4e725c637178b5
Lee <leemcf4@hotmail.co.uk>
2021-01-17 16:35:10 +0000

    deploy

--- backend/utils.js
+++ backend/utils.js
@@ -31,3 +31,14 @@ export const isAuth = (req, res, next)=>{
     }
 }
 
+export const isAdmin = (req, res, next)=>{
+
+    if(req.user && req.user.isAdmin){
+        next();
+    }else{
+        res.status(401).send({message: 'Invalid Token'});
+    }
+
+
+}
+
