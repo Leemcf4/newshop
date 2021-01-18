01259c558cd9d304b0e8f3ecb44478d01d90f5d3
Lee <leemcf4@hotmail.co.uk>
2021-01-18 18:44:48 +0000

    test

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -1,33 +1,33 @@
-import express from "express"
-import expressAsyncHandler from "express-async-handler"
-import Order from "../models/orderModel.js"
-import { isAdmin, isAuth } from "../utils.js"
+import express from "express";
+import expressAsyncHandler from "express-async-handler";
+import Order from "../models/orderModel.js";
+import { isAdmin, isAuth } from "../utils.js";
 
-const orderRouter = express.Router()
+const orderRouter = express.Router();
 orderRouter.get(
   "/",
   isAuth,
   isAdmin,
   expressAsyncHandler(async (req, res) => {
-    const orders = await Order.find({}).populate("user", "name")
-    res.send(orders)
+    const orders = await Order.find({}).populate("user", "name");
+    res.send(orders);
   })
-)
+);
 orderRouter.get(
   "/history",
   isAuth,
   expressAsyncHandler(async (req, res) => {
-    const orders = await Order.find({ user: req.user._id })
-    res.send(orders)
+    const orders = await Order.find({ user: req.user._id });
+    res.send(orders);
   })
-)
+);
 
 orderRouter.post(
   "/",
   isAuth,
   expressAsyncHandler(async (req, res) => {
     if (req.body.orderItems.length === 0) {
-      res.status(400).send({ message: "Cart is empty" })
+      res.status(400).send({ message: "Cart is empty" });
     } else {
       const order = new Order({
         orderItems: req.body.orderItems,
@@ -38,82 +38,82 @@ orderRouter.post(
         taxPrice: req.body.taxPrice,
         totalPrice: req.body.totalPrice,
         user: req.user._id,
-      })
-      const createdOrder = await order.save()
+      });
+      const createdOrder = await order.save();
       res
         .status(201)
-        .send({ message: "New order created", order: createdOrder })
+        .send({ message: "New order created", order: createdOrder });
     }
   })
-)
+);
 
 orderRouter.get(
   "/:id",
   isAuth,
   expressAsyncHandler(async (req, res) => {
-    const order = await Order.findById(req.params.id)
+    const order = await Order.findById(req.params.id);
 
     if (order) {
-      res.send(order)
+      res.send(order);
     } else {
-      res.status(404).send({ message: "Order not found" })
+      res.status(404).send({ message: "Order not found" });
     }
   })
-)
+);
 
 orderRouter.put(
   "/:id/pay",
   isAuth,
   expressAsyncHandler(async (req, res) => {
-    const order = await Order.findById(req.params.id)
+    const order = await Order.findById(req.params.id);
     if (order) {
-      order.isPaid = true
-      order.paidAt = Date.now()
+      order.isPaid = true;
+      order.paidAt = Date.now();
       order.paymentResult = {
         id: req.body.id,
         status: req.body.status,
         update_time: req.body.update_time,
         email_address: req.body.email_address,
-      }
+      };
 
-      const updatedOrder = await order.save()
-      res.send({ message: "Order Paid", order: updatedOrder })
+      const updatedOrder = await order.save();
+      res.send({ message: "Order Paid", order: updatedOrder });
     } else {
-      res.status(404).send({ message: "Order not found" })
+      res.status(404).send({ message: "Order not found" });
     }
   })
-)
+);
 
 orderRouter.delete(
   "/:id",
   isAuth,
   isAdmin,
   expressAsyncHandler(async (req, res) => {
-    const order = await Order.findById(req.params.id)
+    const order = await Order.findById(req.params.id);
     if (order) {
-      const deleteOrder = await order.remove()
-      res.send({ message: "Order Deleted", order: deleteOrder })
+      const deleteOrder = await order.remove();
+      res.send({ message: "Order Deleted", order: deleteOrder });
     } else {
-      res.status(404).send({ message: "Order not found" })
+      res.status(404).send({ message: "Order not found" });
     }
   })
-)
+);
 
 orderRouter.put(
   "/:id/deliver",
   isAuth,
   expressAsyncHandler(async (req, res) => {
-    const order = await Order.findById(req.params.id)
+    const order = await Order.findById(req.params.id);
     if (order) {
-      order.isDelivered = true
-      order.deliveredAt = Date.now()
+      order.isDelivered = true;
+      order.deliveredAt = Date.now();
 
-      const updatedOrder = await order.save()
-      res.send({ message: "Order Delivered", order: updatedOrder })
+      const updatedOrder = await order.save();
+      res.send({ message: "Order Delivered", order: updatedOrder });
     } else {
-      res.status(404).send({ message: "Order not found" })
+      res.status(404).send({ message: "Order not found" });
     }
   })
-)
+);
 
-export default orderRouter
+export default orderRouter;
--- backend/routers/uploadRouter.js
+++ backend/routers/uploadRouter.js
@@ -1,22 +1,22 @@
-import multer from "multer"
-import express from "express"
-import { isAuth } from "../utils.js"
+import multer from "multer";
+import express from "express";
+import { isAuth } from "../utils.js";
 
-const uploadRouter = express.Router()
+const uploadRouter = express.Router();
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, "uploads/")
+    cb(null, "uploads/");
   },
   filename(req, file, cb) {
-    cb(null, `${Date.now()}.jpg`)
+    cb(null, `${Date.now()}.jpg`);
   },
-})
+});
 
-const upload = multer({ storage })
+const upload = multer({ storage });
 
 uploadRouter.post("/", isAuth, upload.single("image"), (req, res) => {
-  res.send(`/${req.file.path}`)
-})
+  res.send(`/${req.file.path}`);
+});
 
-export default uploadRouter
+export default uploadRouter;
